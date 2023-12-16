9de95d4ebb4d3c463a637eb36863e0d1c295e928
Marc raphael <marcraphael979@gmail.com>
2023-12-16 23:01:37 +0100

    Render the view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,29 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import store from './redux/store';
+import Greeting from "./components/Greeting";
+
+function App() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Greeting />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
